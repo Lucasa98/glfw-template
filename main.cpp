5a44b0eca48679e3ec97b2705b4255f0276080ed
lucasa98 <lusaurin@gmail.com>
2025-03-15 21:55:07 -0300

    main test code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-int main(int, char**){
-    std::cout << "Hello, from glfw-template!\n";
+void error_callback(int error, const char* msg) {
+    std::cerr << "GLFW Error (" << error << "): " << msg << std::endl;
+}
+
+int main(){
+    glfwSetErrorCallback(error_callback);
+    if(!glfwInit()) {
+        std::cerr << "GLFW initialization failed" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW Window", nullptr, nullptr);
+    if(window == NULL) {
+        std::cerr << "window creation failed" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "GLAD loading failed" << std::endl;
+        return -1;
+    }
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
