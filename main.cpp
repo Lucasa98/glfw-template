20b3170f618db21f860ba1558d853dd697978be0
lucasa98 <lusaurin@gmail.com>
2025-03-16 03:04:28 -0300

    update main

--- main.cpp
+++ main.cpp
@@ -6,8 +6,19 @@ void error_callback(int error, const char* msg) {
     std::cerr << "GLFW Error (" << error << "): " << msg << std::endl;
 }
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+        glfwSetWindowShouldClose(window, true);
+}
+
 int main(){
     glfwSetErrorCallback(error_callback);
+
+    // init glfw
     if(!glfwInit()) {
         std::cerr << "GLFW initialization failed" << std::endl;
         return -1;
@@ -17,21 +28,34 @@ int main(){
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    // create window
     GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW Window", nullptr, nullptr);
     if(window == NULL) {
         std::cerr << "window creation failed" << std::endl;
         glfwTerminate();
         return -1;
     }
-
     glfwMakeContextCurrent(window);
 
+    // init glad
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cerr << "GLAD loading failed" << std::endl;
         return -1;
     }
 
+    // register callbacks
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetKeyCallback(window, key_callback);
+
+    // Render loop
+    glClearColor(0.2f, 0.3f, 0.3f, 0.1f);
     while(!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // ========== Render ==========
+        
+        // ============================
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
